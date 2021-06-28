9bcdfc42ee69a9418233a00e0499093a061dc548
Grandedev <gabrieldias7200@gmail.com>
2021-06-28 07:48:22 -0300

    Guessing game in Rust

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+
+fn main() {
+    println!("Adivinhe o numero!");
+    
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    // println!("O numero secreto é: {}", secret_number);
+
+    loop {
+
+        println!("Insira aqui seu numero.");
+        
+        let mut guess = String::new();
+        
+        // To summarize, the 
+        // let mut guess = String::new(); line has created
+        // a mutable variable that is currently bound to a new, empty instance of a String. Whew!
+        
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Falhou em ler linha");
+        
+        let guess: u32 = guess
+        .trim()
+        .parse()
+        .expect("Por favor insira um número");
+        
+        println!("Você chutou o numero: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Está muito abaixo!"),
+            Ordering::Greater => println!("Está muito acima!"),
+            Ordering::Equal => {
+                println!("Ganhou!");
+                
+                break;
+            }
+        }
+    }
+
+        
+}
